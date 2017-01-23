7e3c23348424371571d072917fa7ca910395101e
Moritz Kammerer <kammerer.moritz@gmail.com>
2017-01-23 23:48:30 +0100

    Added gRPC.

--- client/src/main/kotlin/de/mkammerer/grpcchat/client/Client.kt
+++ client/src/main/kotlin/de/mkammerer/grpcchat/client/Client.kt
@@ -1,5 +1,17 @@
 package de.mkammerer.grpcchat.client
 
+import de.mkammerer.grpcchat.protocol.GreeterGrpc
+import de.mkammerer.grpcchat.protocol.HelloRequest
+import io.grpc.ManagedChannelBuilder
+
 fun main(args: Array<String>) {
-    println("Client")
+    val channel = ManagedChannelBuilder.forAddress("localhost", 5001)
+            .usePlaintext(true)
+            .build()
+    val blockingStub = GreeterGrpc.newBlockingStub(channel)
+
+    val request = HelloRequest.newBuilder().setName("Client 1").build()
+    val response = blockingStub.sayHello(request)
+
+    println("Got response: ${response.message}")
 }
--- server/src/main/kotlin/de/mkammerer/grpcchat/server/Server.kt
+++ server/src/main/kotlin/de/mkammerer/grpcchat/server/Server.kt
@@ -1,5 +1,28 @@
 package de.mkammerer.grpcchat.server
 
+import de.mkammerer.grpcchat.protocol.GreeterGrpc
+import de.mkammerer.grpcchat.protocol.HelloReply
+import de.mkammerer.grpcchat.protocol.HelloRequest
+import io.grpc.ServerBuilder
+import io.grpc.stub.StreamObserver
+
 fun main(args: Array<String>) {
-    println("Server")
+    val server = ServerBuilder.forPort(5001)
+            .addService(GreeterImpl)
+            .build()
+            .start()
+    Runtime.getRuntime().addShutdownHook(object : Thread() {
+        override fun run() {
+            server.shutdown()
+        }
+    })
+    server.awaitTermination()
+}
+
+object GreeterImpl : GreeterGrpc.GreeterImplBase() {
+    override fun sayHello(req: HelloRequest, responseObserver: StreamObserver<HelloReply>) {
+        val reply = HelloReply.newBuilder().setMessage("Hello " + req.name).build()
+        responseObserver.onNext(reply)
+        responseObserver.onCompleted()
+    }
 }
